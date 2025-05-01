de03871fd4f8b4601b5eca955c1b87f22e60779e
Amandeep <amandeep.nf@gmail.com>
2025-05-01 15:38:32 +0530

    Added edit bank page

--- app/routes/bank-master.router.js
+++ app/routes/bank-master.router.js
@@ -8,4 +8,6 @@ const bankMasterController = require("../controllers/bankMaster.controller.js");
 
 router.get("/get-all-banks", authMiddleware.checkLoginStatus, bankMasterController.getAllBankMaster);
 router.post("/add-bank", authMiddleware.checkLoginStatus, bankMasterController.addBankMaster);
+router.delete("/delete-bank", authMiddleware.checkLoginStatus, bankMasterController.deleteBankMaster);
+router.post("/edit-bank", authMiddleware.checkLoginStatus, bankMasterController.editBankMaster);
 module.exports = router;
